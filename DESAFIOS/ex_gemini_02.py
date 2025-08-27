30defcd8fd8cf936c06e641c549db4956be15c18
alexklenio <alexlopes.see@gmail.com>
2025-08-27 18:53:26 -0300

    Update ex_gemini_02.py

--- DESAFIOS/ex_gemini_02.py
+++ DESAFIOS/ex_gemini_02.py
@@ -4,11 +4,11 @@ def adivinhe():
     comp = randint(1, 10)
     c = 1 
     
-    print('Olá, sou seu computador e pensei em um número entre 1 e 10.')
+    print('\nOlá, sou seu computador e pensei em um número entre 1 e 10.')
     
     while True:
         
-        user = int(input('Adivinhe qual número eu pensei: '))
+        user = int(input('\nAdivinhe qual número eu pensei: '))
 
         if  0 > user > 10:
             print('Número inválido, digite número entre 1 e 10')
@@ -22,7 +22,7 @@ def adivinhe():
             c += 1
             
         else:
-            print(f'Parabéns, você acertou em {c} tentativas!')
+            print(f'\nParabéns, você acertou em {c} tentativas!')
             break 
         
         

794c319c6bd49d81f14d05ffd71d7b3d353554b6
alexklenio <alexlopes.see@gmail.com>
2025-08-31 16:12:58 -0300

    Update ex010.py

--- ATIVIDADES/ex010.py
+++ ATIVIDADES/ex010.py
@@ -3,6 +3,7 @@ from time import sleep
 numero = int(input("Informe um numero para contagem regressiva: "))
 
 for i in range (numero, 0 , -1):
-    print(i, end=' ', flush = True)
-    sleep(0.6)
-   
+    if i % 2 != 0:
+        print(i, end=' ', flush = True)
+        sleep(0.6)
+    

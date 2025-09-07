33148c4ee0084af49349cc51b4b8779e7d619b1f
alexklenio <alexlopes.see@gmail.com>
2025-09-07 20:27:09 -0300

    Update ex07.py

--- DESAFIOS/EX_05_09_2025/ex07.py
+++ DESAFIOS/EX_05_09_2025/ex07.py
@@ -3,19 +3,23 @@ menor = 0
 numeros = []
 
 while cont < 8:
-
     
-    numero = int(input(f"Informe o {cont}º número inteiro: " ))
-    numeros.append(numero)
+        numero = int(input(f"Informe o {cont}º número inteiro: " ))
+  
+        if numero > 0:
+            numeros.append(numero)
 
-    if cont == 1:
-     menor = numero
-     
-    else:
-       if numero < menor:
-          numero = menor
+            if cont == 1:
+                menor = numero
+            
+            else:
+                if numero < menor:
+                    menor = numero
 
-    cont += 1
+            cont += 1
+        else:
+            print("Entrada inválida, os números devem ser positivos")     
+
 
 print(f"\nOs 7 números informados são: {numeros}")
 print(f"Dentre eles {menor} é o menor!")

fb79f467df83165b58facd4c03bf7e2af9bcfe08
alexklenio <alexlopes.see@gmail.com>
2025-09-10 19:19:13 -0300

    Update ex03.py

--- ATIVIDADES/EX_05_09_2025/ex03.py
+++ ATIVIDADES/EX_05_09_2025/ex03.py
@@ -1,9 +1,12 @@
-usuario = str(input("Informe o seu nome de usuário: "))
-senha = str(input("Informe o sua senha: "))
+while True:
 
+    usuario = str(input("Informe o seu nome de usuário: "))
+    senha = str(input("Informe o sua senha: "))
 
-if usuario in senha:
-    print("Sua senha não pode conter partes do seu usuário")
-else:
-    print("Cadastro realizado com sucesso")
+    if usuario in senha:
+        print("Sua senha não pode conter partes do seu usuário")
+
+    else:
+        print("Cadastro realizado com sucesso")
+        break
 

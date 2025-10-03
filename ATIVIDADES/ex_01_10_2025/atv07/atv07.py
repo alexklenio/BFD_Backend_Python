232625fb5c67ef49c144cacc3fe5f16eef273c37
alexklenio <alexlopes.see@gmail.com>
2025-10-03 20:23:35 -0300

    atv07

---
+++ ATIVIDADES/ex_01_10_2025/atv07/atv07.py
@@ -0,0 +1,23 @@
+class Cachorro:
+    def __init__(self, nome, cor_pelo, raca):
+        self.nome = nome
+        self.cor_pelo = cor_pelo
+        self.raca = raca
+
+    def latir(self):
+        print(f'{self.nome} está latindo')
+
+    def comer(self):
+        print(f'{self.nome} está comendo, cuidado!')
+
+    def correr(self):
+        print(f'{self.nome} está correndo!')
+
+meu_cachorro=Cachorro('Rex', 'Caramelo', 'Vira-lata')
+
+print(f'O nome do meu cachorro é {meu_cachorro.nome}, de cor {meu_cachorro.cor_pelo} e é da raça {meu_cachorro.raca}\n')
+
+meu_cachorro.comer()
+meu_cachorro.correr()
+meu_cachorro.latir()
+

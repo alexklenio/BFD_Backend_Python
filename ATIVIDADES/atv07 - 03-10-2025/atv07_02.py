c041d257d23f405b540f63bfdd472151265a98e1
alexklenio <alexlopes.see@gmail.com>
2025-10-05 22:54:53 -0300

    atv07

--- ATIVIDADES/atv07 - 03-10-2025/atv07_02.py
+++ ATIVIDADES/atv07 - 03-10-2025/atv07_02.py
@@ -1,6 +1,6 @@
 class Animal:
     
-    def __init__(self,  nome, cor_pelo, raca):
+    def __init__(self,  nome, raca, cor_pelo):
         self.nome = nome
         self.cor_pelo = cor_pelo
         self.raca = raca
@@ -14,16 +14,17 @@ class Animal:
 
 class Cachorro(Animal):
 
-    def __init__(self, nome, cor_pelo, raca):
+    def __init__(self, nome, raca, cor_pelo, tamanho_do_pelo):
        super().__init__(nome, cor_pelo, raca)
+       self.tamanho_do_pelo = tamanho_do_pelo
 
     def latir(self):
         print(f'{self.nome} está latindo')
 
 
-meu_cachorro=Cachorro('Rex', 'Caramelo', 'Vira-lata')
+meu_cachorro=Cachorro('Rex', 'Vira-lata','Caramelo', 'baixo')
 
-print(f'O nome do meu cachorro é {meu_cachorro.nome}, de cor {meu_cachorro.cor_pelo} e é da raça {meu_cachorro.raca}\n')
+print(f'O nome do meu cachorro é {meu_cachorro.nome}, de cor {meu_cachorro.cor_pelo}, é da raça {meu_cachorro.raca} e o seu pelo é {meu_cachorro.tamanho_do_pelo}\n')
 
 meu_cachorro.comer()
 meu_cachorro.correr()

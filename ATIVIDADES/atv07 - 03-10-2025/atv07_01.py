8cf72096f445e2cf3136153bdf7c6eff2591b09e
alexklenio <alexlopes.see@gmail.com>
2025-10-07 21:10:57 -0300

    exercicio

--- ATIVIDADES/atv07 - 03-10-2025/atv07_01.py
+++ ATIVIDADES/atv07 - 03-10-2025/atv07_01.py
@@ -1,8 +1,11 @@
+
 class Cachorro:
     def __init__(self, nome, cor_pelo, raca):
+
         self.nome = nome
         self.cor_pelo = cor_pelo
         self.raca = raca
+        
 
     def latir(self):
         print(f'{self.nome} está latindo')
@@ -13,11 +16,17 @@ class Cachorro:
     def correr(self):
         print(f'{self.nome} está correndo!')
 
-meu_cachorro=Cachorro('Rex', 'Caramelo', 'Vira-lata')
 
-print(f'O nome do meu cachorro é {meu_cachorro.nome}, de cor {meu_cachorro.cor_pelo} e é da raça {meu_cachorro.raca}\n')
+meu_cachorro=Cachorro('Rex', 'Caramelo', 'Vira-lata')
+dog = Cachorro("salsicha", "Caramelo", "Baset")
+
+
+print(f'\nO nome do meu cachorro é {meu_cachorro.nome}, de cor {meu_cachorro.cor_pelo} e é da raça {meu_cachorro.raca}\n')
+
+print(f'Meu segundo animal também é um cachorro! Seu nome é {dog.nome}, seu pelo também é {dog.cor_pelo}, mas a sua raça é {dog.raca}\n')
 
 meu_cachorro.comer()
-meu_cachorro.correr()
-meu_cachorro.latir()
+dog.comer()
 
+meu_cachorro.latir()
+dog.correr()
--- ATIVIDADES/atv07 - 03-10-2025/atv07_02.py
+++ ATIVIDADES/atv07 - 03-10-2025/atv07_02.py
@@ -5,7 +5,6 @@ class Mamifero:
         self.raca = raca
         self.dieta = dieta
 
-
     def comer(self):
         print(f'{self.nome} está comendo.')
 
@@ -22,16 +21,26 @@ class Cachorro(Mamifero):
        super().__init__(nome, raca, dieta)
        self.cor_pelo = cor_pelo
        self.tamanho_do_pelo = tamanho_do_pelo
-
+       
 
     def latir(self):
         print(f'{self.nome} está latindo')
 
+class Morcego(Mamifero):
+    def __init__(self, nome, raca, dieta):
+        super().__init__(nome, raca, dieta)
+
+   
 
 dog= Cachorro('Rex', 'Vira-lata','Onívoro','Caramelo','baixo')
+dog2 = Cachorro('João', 'Poodle', 'Onívoro', 'Malhado', 'Alto')
+bat = Morcego('batman', 'cinza', 'onivora' )
 
 print(f'O nome do meu cachorro é {dog.nome}, de cor {dog.cor_pelo}, é da raça {dog.raca} e o seu pelo é {dog.tamanho_do_pelo}\n')
 
-dog.comer()
-dog.correr()
+print(f'{dog2.nome} tem a cor {dog2.cor_pelo} e é da raça {dog2.raca}')
+
+dog.dormir()
+dog2.correr()
 dog.latir()
+dog2.latir()
